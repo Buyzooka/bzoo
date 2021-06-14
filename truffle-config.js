d1aa15b0ae1a43b54dde2a38bdee6395ecd636ab
JuJu <julien.gabriel2@gmail.com>
2021-06-14 14:31:51 +0200

    Added configuration to deploy on ETHEREUM mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
+require('dotenv').config();
 
 module.exports = {
     networks: {
@@ -8,6 +9,13 @@ module.exports = {
             host: "127.0.0.1",     // Localhost (default: none)
             port: 8545,            // Standard Ethereum port (default: none)
             network_id: "*",       // Any network (default: none)
+        },
+        mainnet: {
+            provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${env.INFURA_API_KEY}`),
+            network_id: 80001,
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true
         },
         mumbai: {
             provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
@@ -17,6 +25,13 @@ module.exports = {
             skipDryRun: true
         },
         matic: {
+            provider: () => new HDWalletProvider(mnemonic, `https://rpc-mainnet.maticvigil.com/`),
+            network_id: 137,
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true
+        },
+        polygon: {
             provider: () => new HDWalletProvider(mnemonic, `https://rpc-mainnet.maticvigil.com/`),
             network_id: 137,
             confirmations: 2,
